a6817bf9a27335f0a399742632f7eca3a3575125
naunidh05 <73524513+naunidh05@users.noreply.github.com>
2021-05-03 15:08:25 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,22 @@
+class Slingshot{
+    constructor(bodyA,pointB){
+        var options={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.01,
+            length:10
+        }
+        this.pointB=pointB
+        this.slingshot=Constraint.create(options);
+        World.add(world,this.Slingshot)
+    }
+    fly(){
+        this.slingshot.bodyA=null;
+     }
+     Launch(bodyA){
+         this.slingshot.bodyA=bodyA;
+     }
+     display(){
+         
+     }
+}
